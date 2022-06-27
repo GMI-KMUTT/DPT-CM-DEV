f31c2c7ee75b6fd83d00c0c8c43674ae3ed367fa
kritsanapr <kritsana.pr.41@gmail.com>
2022-06-27 23:29:44 +0700

    [Config] Organize project structure

--- apps/app-cm/src/app/app.module.ts
+++ apps/app-cm/src/app/app.module.ts
@@ -4,12 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from "./share/materials/materials.module";
-
+import { MaterialModule } from './share/materials/materials.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RoutingModule } from './routing/routing.module';
+import { ProjectComponent } from './pages/project/project.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, MaterialModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    NavbarComponent,
+    DashboardComponent,
+    LoginComponent,
+    SidebarComponent,
+    ProjectComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, MaterialModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/app-cm/src/app/routing/routing.module.ts
+++ apps/app-cm/src/app/routing/routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../pages/login/login.component';
+import { ProjectComponent } from '../pages/project/project.component';
 
-
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'project', component: ProjectComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class RoutingModule { }
+export class RoutingModule {}
